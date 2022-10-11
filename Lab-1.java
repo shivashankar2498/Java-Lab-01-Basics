88b14d743bb8f611a250bad3df31942649fa9aef
DevOps-Cloud-FullStack <103547000+MuralidharFullstack@users.noreply.github.com>
2022-10-11 23:54:25 +0530

    lab1 for practice

---
+++ Lab-1.java
@@ -0,0 +1,987 @@
+# 1
+ class A1
+ {
+ public static void main(String[] args) 
+ {
+ System.out.println("Hello World!");
+ }
+ }
+
+class A2
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+}
+
+
+
+class A3
+{
+	public static void main(String[] args) 
+	{
+		System.out.print(1);
+		System.out.print(2);
+		System.out.print(3);
+		System.out.println();
+		System.out.print(4);
+		System.out.print(5);
+		System.out.print(6);
+		System.out.println();
+	}
+}
+
+class A4
+{
+	public static void main(String[] args) 
+	{
+		System.out.print(9);
+		System.out.print(8);
+		System.out.print(7);
+		System.out.print(6);
+		System.out.print(5);
+	}
+}
+
+
+class A5
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+	}
+}
+
+
+class A6
+{
+	public static void main(String[] args) 
+	{
+		double d1 = 1.5;
+		double d2 = 2.5;
+		System.out.println(d1);
+		System.out.println(d2);
+		d1 = 4.5;
+		System.out.println(d1);
+		System.out.println(d2);
+		d2 = 5.5;
+		System.out.println(d1);
+		System.out.println(d2);
+	}
+}
+
+
+class A6
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		System.out.println(i);
+	}
+}
+
+class A7
+{
+	public static void main(String[] args) 
+	{
+		int i = 1;
+		System.out.println(i);
+		i = 2;
+		System.out.println(i);
+	}
+}
+
+class A8
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		System.out.println(i);
+		i = 20;
+		System.out.println(i);
+		i = 2;
+		System.out.println(i);
+		i = 4;
+		System.out.println(i);
+	}
+}
+
+class A9
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		i = 50;
+		System.out.println(i);
+		i = 40;
+		System.out.println(i);
+		i = 10;
+		System.out.println(i);
+	}
+}
+
+class A10
+{
+	public static void main(String[] args) 
+	{
+		boolean b1 = true;
+		System.out.println(b1);
+		b1 = false;
+		System.out.println(b1);
+		b1 = true;
+		b1 = false;
+		b1 = true;
+		System.out.println(b1);
+	}
+}
+class A11
+{
+	public static void main(String[] args) 
+	{
+		double var = 4.5;
+		System.out.println(var);
+		var = -4.9;
+		System.out.println(var);
+		var = 54.9;
+		System.out.println(var);
+		var = -0.9;
+		System.out.println(var);
+	}
+}
+
+
+
+class A12
+{
+	public static void main(String[] args) 
+	{
+		System.out.print(1);
+		System.out.print(2);
+		System.out.print(3);
+		System.out.println();
+		System.out.print(4);
+		System.out.print(5);
+		System.out.print(6);
+		System.out.println();
+	}
+}
+
+class A13
+{
+	public static void main(String[] args) 
+	{
+		System.out.print(9);
+		System.out.print(8);
+		System.out.print(7);
+		System.out.print(6);
+		System.out.print(5);
+	}
+}
+
+
+class A14
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+	}
+}
+
+
+class A15
+{
+	public static void main(String[] args) 
+	{
+		double d1 = 1.5;
+		double d2 = 2.5;
+		System.out.println(d1);
+		System.out.println(d2);
+		d1 = 4.5;
+		System.out.println(d1);
+		System.out.println(d2);
+		d2 = 5.5;
+		System.out.println(d1);
+		System.out.println(d2);
+	}
+}
+
+
+class A16
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		System.out.println(i);
+	}
+}
+
+class A17
+{
+	public static void main(String[] args) 
+	{
+		int i = 1;
+		System.out.println(i);
+		i = 2;
+		System.out.println(i);
+	}
+}
+
+class A18
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		System.out.println(i);
+		i = 20;
+		System.out.println(i);
+		i = 2;
+		System.out.println(i);
+		i = 4;
+		System.out.println(i);
+	}
+}
+
+class A19
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		i = 50;
+		System.out.println(i);
+		i = 40;
+		System.out.println(i);
+		i = 10;
+		System.out.println(i);
+	}
+}
+
+class A20
+{
+	public static void main(String[] args) 
+	{
+		boolean b1 = true;
+		System.out.println(b1);
+		b1 = false;
+		System.out.println(b1);
+		b1 = true;
+		b1 = false;
+		b1 = true;
+		System.out.println(b1);
+	}
+}
+class A21
+{
+	public static void main(String[] args) 
+	{
+		double var = 4.5;
+		System.out.println(var);
+		var = -4.9;
+		System.out.println(var);
+		var = 54.9;
+		System.out.println(var);
+		var = -0.9;
+		System.out.println(var);
+	}
+}
+
+class A22
+{
+	public static void main(String[] args) 
+	{
+		char c1 = 'r';
+		System.out.println(c1);
+		c1 = 'a';
+		System.out.println(c1);
+		c1 = '6';
+		System.out.println(c1);
+		c1 = '&';
+		System.out.println(c1);
+	}
+}
+
+class A23
+{
+	public static void main(String[] args) 
+	{
+		char var;
+		var = 'R';
+		System.out.println(var);
+		var = 'e';
+		System.out.println(var);
+		var = '7';
+		System.out.println(var);
+	}
+}
+
+
+class 24
+{
+	public static void main(String[] args) 
+	{
+		String str = "hello";
+		System.out.println(str);
+		str = "India";
+		System.out.println(str);
+	}
+}
+class A25
+{
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 90;
+		System.out.println(a);
+		System.out.println(b);
+		a = 1000;
+		b = 50;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+==================================
+
+
+class A26
+{
+	public static void main(String[] args) 
+	{
+		int i = 1, j = 2, k = 3;
+		System.out.println(i + ", " + j + ", " + k);
+		System.out.println("i = " + i + ", j = " + j + ", k = " + k);
+	}
+}
+
+
+class A27
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		int j = 20;
+		int m = 10, n = 20;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(m);
+		System.out.println(n);
+	}
+}
+
+class A28
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		double j = 4.5;
+		char k = 'y';
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+	}
+}
+
+class A29
+{
+	public static void main(String[] args) 
+	{
+		double d1 = 1.5, d2 = 5.5, d3 = 10.6;
+		int i = 10, j = 20, k = 30;
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(d3);
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+	}
+}
+
+
+class A30
+{
+	public static void main(String[] args) 
+	{
+		int i = 1, j = 3, k = 60;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+	}
+}
+
+
+class A31 
+{
+	public static void main(String[] args) 
+	{
+		int i = 10, j = 10, k = 10, m = 10;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(m);
+	}
+}
+
+class A32
+{
+	public static void main(String[] args) 
+	{
+		char c1, c2, c3;
+		c1 = 'y';
+		c2 = 't';
+		c3 = 'R';
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+	}
+}
+class A33
+{
+	public static void main(String[] args) 
+	{
+		int i, j = 1, k;
+		i = 10;
+		k = 40;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+	}
+}
+
+class A34
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		System.out.println(i);
+		System.out.println("i = " + i);
+		System.out.println("i : " + i);
+	}
+}
+
+class A35
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		int j = 20;
+		System.out.println(i);
+		System.out.println(j);
+		i = j = 40;
+		System.out.println(i);
+		System.out.println(j);
+	}
+}
+
+class A36
+{
+	public static void main(String[] args) 
+	{
+		int i, j, k, m;
+		i = j = k = m = 10;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(m);
+	}
+}
+class A37
+{
+	public static void main(String[] args) 
+	{
+		double weight = 67.5;
+		System.out.println(weight);
+		System.out.println(weight + " is weight value");
+		System.out.println(weight + " ==> weight value");
+		System.out.println(weight + " --> weight value");
+	}
+}
+
+class A38
+{
+	public static void main(String[] args) 
+	{
+		int age = 25;
+		System.out.println(age);
+		System.out.println("age is :" + age);
+		System.out.println("age = " + age);
+		System.out.println("age : " + age);
+		System.out.println("age value is : " + age);
+	}
+}
+
+class A39
+{
+	public static void main(String[] args) 
+	{
+		int i = 10, j = 20;
+		System.out.println(i + ", ");
+		System.out.println(i + ", " + j);
+	}
+}
+
+class A40
+{
+	public static void main(String[] args) 
+	{
+		int i = 10, j = 20;
+		System.out.println("i = " + i);
+		System.out.println("i = " + i + ", ");
+		System.out.println("i = " + i + ", j = ");
+		System.out.println("i = " + i + ", j = " + j);
+	}
+}
+
+
+class A41
+{
+	public static void main(String[] args) 
+	{
+		int i = 100;
+		double i = 4.5;
+		System.out.println("done");
+	}
+}
+
+class A42
+{
+	public static void main(String[] args) 
+	{
+		int i = true;
+		System.out.println("done");
+	}
+}
+
+
+class A43
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		System.out.println(i);
+	}
+}
+
+class A44
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		int i = 20;
+		System.out.println("done");
+	}
+}
+
+class A45
+{
+	public static void main(String[] args) 
+	{
+		int i = 100;
+		double i = 4.5;
+		System.out.println("done");
+	}
+}
+
+==============
+
+class A46
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+
+
+class A47
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+}
+
+
+class A48 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Hello World!");
+		System.out.print("Hello World!");
+		System.out.print("Hello World!");
+	}
+}
+
+class A49
+{
+	public static void main(String[] args) 
+	{
+		System.out.print(1);
+		System.out.print(2);
+		System.out.print(3);
+		System.out.println();
+		System.out.print(4);
+		System.out.print(5);
+		System.out.print(6);
+		System.out.println();
+	}
+}
+
+class A50
+{
+	public static void main(String[] args) 
+	{
+		System.out.print(9);
+		System.out.print(8);
+		System.out.print(7);
+		System.out.print(6);
+		System.out.print(5);
+	}
+}
+
+
+class A51
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+	}
+}
+
+
+class A52
+{
+	public static void main(String[] args) 
+	{
+		double d1 = 1.5;
+		double d2 = 2.5;
+		System.out.println(d1);
+		System.out.println(d2);
+		d1 = 4.5;
+		System.out.println(d1);
+		System.out.println(d2);
+		d2 = 5.5;
+		System.out.println(d1);
+		System.out.println(d2);
+	}
+}
+
+
+class A53
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		System.out.println(i);
+	}
+}
+
+class A54
+{
+	public static void main(String[] args) 
+	{
+		int i = 1;
+		System.out.println(i);
+		i = 2;
+		System.out.println(i);
+	}
+}
+
+class A55
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		System.out.println(i);
+		i = 20;
+		System.out.println(i);
+		i = 2;
+		System.out.println(i);
+		i = 4;
+		System.out.println(i);
+	}
+}
+
+class A56
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		i = 50;
+		System.out.println(i);
+		i = 40;
+		System.out.println(i);
+		i = 10;
+		System.out.println(i);
+	}
+}
+
+class A57
+{
+	public static void main(String[] args) 
+	{
+		boolean b1 = true;
+		System.out.println(b1);
+		b1 = false;
+		System.out.println(b1);
+		b1 = true;
+		b1 = false;
+		b1 = true;
+		System.out.println(b1);
+	}
+}
+class A58 
+{
+	public static void main(String[] args) 
+	{
+		double var = 4.5;
+		System.out.println(var);
+		var = -4.9;
+		System.out.println(var);
+		var = 54.9;
+		System.out.println(var);
+		var = -0.9;
+		System.out.println(var);
+	}
+}
+
+class A59
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+
+
+class 60
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+}
+
+
+class A61
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Hello World!");
+		System.out.print("Hello World!");
+		System.out.print("Hello World!");
+	}
+}
+
+class A62
+{
+	public static void main(String[] args) 
+	{
+		System.out.print(1);
+		System.out.print(2);
+		System.out.print(3);
+		System.out.println();
+		System.out.print(4);
+		System.out.print(5);
+		System.out.print(6);
+		System.out.println();
+	}
+}
+
+class A63
+{
+	public static void main(String[] args) 
+	{
+		System.out.print(9);
+		System.out.print(8);
+		System.out.print(7);
+		System.out.print(6);
+		System.out.print(5);
+	}
+}
+
+
+class A64
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+		System.out.print("hello");
+	}
+}
+
+
+class A65 
+{
+	public static void main(String[] args) 
+	{
+		double d1 = 1.5;
+		double d2 = 2.5;
+		System.out.println(d1);
+		System.out.println(d2);
+		d1 = 4.5;
+		System.out.println(d1);
+		System.out.println(d2);
+		d2 = 5.5;
+		System.out.println(d1);
+		System.out.println(d2);
+	}
+}
+
+
+class A67
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		System.out.println(i);
+	}
+}
+
+class A68 
+{
+	public static void main(String[] args) 
+	{
+		int i = 1;
+		System.out.println(i);
+		i = 2;
+		System.out.println(i);
+	}
+}
+
+class A69
+{
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		System.out.println(i);
+		i = 20;
+		System.out.println(i);
+		i = 2;
+		System.out.println(i);
+		i = 4;
+		System.out.println(i);
+	}
+}
+
+class A70
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		i = 50;
+		System.out.println(i);
+		i = 40;
+		System.out.println(i);
+		i = 10;
+		System.out.println(i);
+	}
+}
+
+class A71
+{
+	public static void main(String[] args) 
+	{
+		boolean b1 = true;
+		System.out.println(b1);
+		b1 = false;
+		System.out.println(b1);
+		b1 = true;
+		b1 = false;
+		b1 = true;
+		System.out.println(b1);
+	}
+}
+class A72
+{
+	public static void main(String[] args) 
+	{
+		double var = 4.5;
+		System.out.println(var);
+		var = -4.9;
+		System.out.println(var);
+		var = 54.9;
+		System.out.println(var);
+		var = -0.9;
+		System.out.println(var);
+	}
+}
+
+class A73
+{
+	public static void main(String[] args) 
+	{
+		char c1 = 'r';
+		System.out.println(c1);
+		c1 = 'a';
+		System.out.println(c1);
+		c1 = '6';
+		System.out.println(c1);
+		c1 = '&';
+		System.out.println(c1);
+	}
+}
+
+class A74
+{
+	public static void main(String[] args) 
+	{
+		char var;
+		var = 'R';
+		System.out.println(var);
+		var = 'e';
+		System.out.println(var);
+		var = '7';
+		System.out.println(var);
+	}
+}
+
+
+class A75
+{
+	public static void main(String[] args) 
+	{
+		String str = "hello";
+		System.out.println(str);
+		str = "India";
+		System.out.println(str);
+	}
+}
+class A76
+{
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 90;
+		System.out.println(a);
+		System.out.println(b);
+		a = 1000;
+		b = 50;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+==================================
